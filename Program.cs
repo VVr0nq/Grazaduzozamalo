852c5d371fcce7b9baf72f1e5696f8f5a5b3728a
VVr0nq <arek.migocki@gmail.com>
2022-06-19 11:43:39 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using Grazaduzozamalo.Model;
+
+namespace Grazaduzozamalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new KontrolerCLI()).Uruchom();
+        }
+
+    }
+}
